d4c3642a5a3bc1a0ce1570b81362a6d011c07379
YLG29fr <131044796+YLG29fr@users.noreply.github.com>
2025-05-28 11:18:13 +0200

    first commit, set Vue TS

---
+++ src/components/HeroBanner.vue
@@ -0,0 +1,30 @@
+<!-- src/components/HelloWorld.vue -->
+
+<script setup lang="ts">
+
+defineProps<{ title: string, msg: string }>()
+
+import logoBanner from '../assets/logo.png'
+
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <h2>{{ msg }}</h2>
+
+  <img :src="logoBanner" alt="alt de la banner" class="hoverEffect"/>
+
+</template>
+
+<style scoped>
+.hoverEffect {
+  filter: drop-shadow(0 0 2em #646cffaa);
+  transition: ease-in-out 0.5s;
+  width: 50%;
+}
+.hoverEffect:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+  transition: ease-in-out 0.5s;
+  transform: scale(1.2);
+}
+</style>
