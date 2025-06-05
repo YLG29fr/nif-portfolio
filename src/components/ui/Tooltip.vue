f47f4414373c884d2e6344c097d7b4e7e80c82e0
YLG29fr <131044796+YLG29fr@users.noreply.github.com>
2025-06-05 17:25:12 +0200

    le tooltip ne déborde plus, revoir par la suite

--- src/components/ui/Tooltip.vue
+++ src/components/ui/Tooltip.vue
@@ -2,7 +2,10 @@
 <template>
     <div class="tooltip-container" @mouseenter="show = true" @mouseleave="show = false">
         <slot></slot>
-        <div v-if="show" class="tooltip-content">
+        <div v-if="show" 
+        class="tooltip-content"
+        ref="tooltipRef"
+        >
             <p class="has-text-centered">
                 <b>{{ title }}</b>
             </p>
@@ -12,9 +15,40 @@
 </template>
 
 <script setup lang="ts">
-    import { ref } from 'vue'
+
+    import { ref, watch, nextTick } from 'vue'
     defineProps<{ text: string , title: string}>()
     const show = ref(false)
+
+    const tooltipRef = ref<HTMLElement | null>(null)
+    const adjustPosition = () => {
+        if (!tooltipRef.value) return
+        const rect = tooltipRef.value.getBoundingClientRect()
+        const padding = 1
+
+        // Si le tooltip dépasse à gauche
+        if (rect.left < padding) {
+            tooltipRef.value.style.left = `${padding - rect.left + 10}%`
+            tooltipRef.value.style.transform = 'translateX(0)'
+        }
+        // Si le tooltip dépasse à droite
+        else if (rect.right > window.innerWidth - padding) {
+            const over = rect.right - window.innerWidth + padding
+            tooltipRef.value.style.left = `calc(10% - ${over}px)`
+            tooltipRef.value.style.transform = 'translateX(-100%)'
+        } else {
+            tooltipRef.value.style.left = '10%'
+            tooltipRef.value.style.transform = 'translateX(-50%)'
+        }
+}
+
+watch(show, async (val) => {
+    if (val) {
+        await nextTick()
+        adjustPosition()
+    }
+})
+
 </script>
 
 <style scoped>
