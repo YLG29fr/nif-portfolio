d2f13697120af08428fc49efbccb3c79fbb66d9a
YLG29fr <131044796+YLG29fr@users.noreply.github.com>
2025-07-02 16:54:21 +0200

    modif navigation-router vite pour github pages

--- src/router.ts
+++ src/router.ts
@@ -24,7 +24,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory('/nif-portfolio/'),
     routes,
     scrollBehavior(savedPosition) {
         if (savedPosition) {
